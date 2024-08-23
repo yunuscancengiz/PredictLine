1bf8bceb39307aa6048200b8c00dbf309da66a36
Yunus Can Cengiz <yunuscancengizz@gmail.com>
2024-08-23 14:27:42 +0300

    try-except blocks commented out to see the detailed error message

--- write_to_influx.py
+++ write_to_influx.py
@@ -38,39 +38,39 @@ class InfluxDBWriter:
 
 
     def connect(self):
-        try:
-            if self.username != None and self.password != None:
-                self.logger.info(msg='Database client created!')
-                return InfluxDBClient(host=self.host, port=self.port, username=self.username, password=self.password)
-            else:
-                self.logger.info(msg='Database client created!')
-                return InfluxDBClient(host=self.host, port=self.port)
-        except Exception as e:
+        #try:
+        if self.username != None and self.password != None:
+            self.logger.info(msg='Database client created!')
+            return InfluxDBClient(host=self.host, port=self.port, username=self.username, password=self.password)
+        else:
+            self.logger.info(msg='Database client created!')
+            return InfluxDBClient(host=self.host, port=self.port)
+        """except Exception as e:
             self.logger.error(msg=f'Exception happened when creating database client! Error message: {e}')
-            self.logger.error(msg=traceback.format_exc())
+            self.logger.error(msg=traceback.format_exc())"""
 
 
     def create_and_switch_database(self):
         if self.is_exist == True:
-            try:
-                self.client.switch_database(database=self.dbname)
-                self.logger.info(msg=f'Database switched into {self.dbname} successfuly!')
-            except Exception as e:
+            #try:
+            self.client.switch_database(database=self.dbname)
+            self.logger.info(msg=f'Database switched into {self.dbname} successfuly!')
+            """except Exception as e:
                 self.logger.error(msg=f'Exception happened when switching into {self.dbname} named database! Error message: {e}')
-                self.logger.error(traceback.format_exc())
+                self.logger.error(traceback.format_exc())"""
         else:
-            try:
-                self.client.create_database(dbname=self.dbname)
-                self.logger.info(msg=f'{self.dbname} named database created successfuly!')
-            except Exception as e:
+            #try:
+            self.client.create_database(dbname=self.dbname)
+            self.logger.info(msg=f'{self.dbname} named database created successfuly!')
+            """except Exception as e:
                 self.logger.error(f'Exception happened when creating {self.dbname} named database! Error message: {e}')
-                self.logger.error(traceback.format_exc())
-            try:
-                self.client.switch_database(database=self.dbname)
-                self.logger.info(msg=f'Database switched into {self.dbname} successfuly!')
-            except Exception as e:
+                self.logger.error(traceback.format_exc())"""
+            #try:
+            self.client.switch_database(database=self.dbname)
+            self.logger.info(msg=f'Database switched into {self.dbname} successfuly!')
+            """except Exception as e:
                 self.logger.error(f'Exception happened when switching into {self.dbname} named database! Error message: {e}')
-                self.logger.error(traceback.format_exc())
+                self.logger.error(traceback.format_exc())"""
 
 
     def write_data(self, data):
@@ -78,18 +78,18 @@ class InfluxDBWriter:
 
 
     def delete_database(self, force:bool):
-        try:
-            if force == False:
-                self.logger.warning(msg=f'{self.dbname} named database is going to be deleted! Do you want to continue? (y/n)')
-                choice = input('')
-                if choice.lower == 'n':
-                    self.logger.info(msg=f'Drop {self.dbname} database process canceled!')
-                    return
-            self.client.drop_database(dbname=self.dbname)
-            self.logger.info(msg=f'{self.dbname} named database dropped successfuly!')
-        except Exception as e:
+        #try:
+        if force == False:
+            self.logger.warning(msg=f'{self.dbname} named database is going to be deleted! Do you want to continue? (y/n)')
+            choice = input('')
+            if choice.lower == 'n':
+                self.logger.info(msg=f'Drop {self.dbname} database process canceled!')
+                return
+        self.client.drop_database(dbname=self.dbname)
+        self.logger.info(msg=f'{self.dbname} named database dropped successfuly!')
+        """except Exception as e:
             self.logger.error(msg=f'Exception happened when dropping the {self.dbname} named database! Error message: {e}')
-            self.logger.error(traceback.format_exc())
+            self.logger.error(traceback.format_exc())"""
 
     def fetch_data(self, query:str) -> pd.DataFrame:
         # @TODO: examine the type of the data that query() func returns
@@ -102,7 +102,7 @@ class InfluxDBWriter:
 
 if __name__ == '__main__':
     db_client = InfluxDBWriter(
-        host='34.45.38.113',
+        host='localhost',
         port=8086,
         dbname='test_db',
         is_exist=False
