958d818f4f1555e4f1e81be70ccffb680a449532
Yunus Can Cengiz <yunuscancengizz@gmail.com>
2024-12-04 18:41:24 +0300

    run the main code for 15m model

--- main.py
+++ main.py
@@ -72,44 +72,46 @@ class RunPipeline:
 
     def pipeline(self):
         # calculate starting and ending dates
-        self.starting_date = str((datetime.now() - timedelta(days=1)).isoformat()).split('T')[0] + 'T00:00:00Z'
-        self.ending_date = str((datetime.now() - timedelta(days=1)).isoformat()).split('T')[0] + 'T23:59:00Z'
+        #self.starting_date_1m = str((datetime.now() - timedelta(days=14)).isoformat()).split('T')[0] + 'T00:00:00Z'
+        #self.ending_date_1m = str((datetime.now() - timedelta(days=1)).isoformat()).split('T')[0] + 'T23:59:00Z'
+        self.starting_date_15m = str((datetime.now() - timedelta(days=90)).isoformat()).split('T')[0] + 'T00:00:00Z'
+        self.ending_date_15m = str((datetime.now() - timedelta(days=1)).isoformat()).split('T')[0] + 'T23:45:00Z'
 
         # create dataset
-        raw_df_1m = self.dataset_creator.main(start=self.starting_date, stop=self.ending_date, line='L301', timeframe='1m', machine='Blower-Pump-1')
-        raw_df_15m = self.dataset_creator.main(start=self.starting_date, stop=self.ending_date, line='L301', timeframe='15m', machine='Blower-Pump-1')
+        #raw_df_1m = self.dataset_creator.main(start=self.starting_date_1m, stop=self.ending_date_1m, line='L301', timeframe='1m', machine='Blower-Pump-1')
+        raw_df_15m = self.dataset_creator.main(start=self.starting_date_15m, stop=self.ending_date_15m, line='L301', timeframe='15m', machine='Blower-Pump-1')
 
         # produce raw data
-        self.producer.main(topic='raw-data', df=raw_df_1m)
+        #self.producer.main(topic='raw-data', df=raw_df_1m)
         self.producer.main(topic='raw-data-15m', df=raw_df_15m)
 
         # fetch raw data from druid
         t.sleep(15)  # wait for druid to consume the raw data from kafka topics
-        df_1m = self.druid_fetcher.main(topic='raw-data')
+        #df_1m = self.druid_fetcher.main(topic='raw-data')
         df_15m = self.druid_fetcher.main(topic='raw-data-15m')
 
         # pre-process data
-        processed_df_1m = self.preprocesser.main(df=df_1m)
+        #processed_df_1m = self.preprocesser.main(df=df_1m)
         processed_df_15m = self.preprocesser.main(df=df_15m)
         # produce processed data
-        self.producer.main(topic='processed-data', df=processed_df_1m)
+        #self.producer.main(topic='processed-data', df=processed_df_1m)
         self.producer.main(topic='processed-data-15m', df=processed_df_15m)
 
         # fetch processed data from druid
         t.sleep(15)  # wait for druid to consume the processed data from kafka topics
-        df_1m = self.druid_fetcher.main(topic='processed-data')
+        #df_1m = self.druid_fetcher.main(topic='processed-data')
         df_15m = self.druid_fetcher.main(topic='processed-data-15m')
 
         # run lstm model
-        results_1m, predicted_data_1m = self.lstm_model.main(df=df_1m, input_days=14, output_days=2, interval_minute=1)
+        #results_1m, predicted_data_1m = self.lstm_model.main(df=df_1m, input_days=14, output_days=2, interval_minute=1)
         results_15m, predicted_data_15m = self.lstm_model.main(df=df_15m, input_days=90, output_days=10, interval_minute=15)
 
         # produce predicted data 
-        self.producer.main(topic='predicted-data', df=predicted_data_1m)
+        #self.producer.main(topic='predicted-data', df=predicted_data_1m)
         self.producer.main(topic='predicted-data-15m', df=predicted_data_15m)
 
         # insert model results into postgre db
-        self.postgre_client.insert_data(table_name='model_results_1m', results=results_1m)
+        #self.postgre_client.insert_data(table_name='model_results_1m', results=results_1m)
         self.postgre_client.insert_data(table_name='model_results_15m', results=results_15m)       
 
 
