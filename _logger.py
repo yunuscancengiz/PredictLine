81bbb9f7efeda5ebf18a0b6af907c07cfd2923e8
Yunus Can Cengiz <yunuscancengizz@gmail.com>
2024-12-06 22:39:31 +0300

    bug fix

--- _logger.py
+++ _logger.py
@@ -1,4 +1,20 @@
 import logging
+from colorama import Fore, Style
+
+
+class ColorFormatter(logging.Formatter):
+    COLOR_MAP = {
+        logging.INFO: Fore.BLUE,  # Info messages in blue
+        logging.WARNING: Fore.YELLOW,  # Warning messages in yellow
+        logging.ERROR: Fore.RED,  # Error messages in red
+        logging.DEBUG: Fore.GREEN,  # Optional: Debug messages in green
+        logging.CRITICAL: Fore.MAGENTA,  # Optional: Critical messages in magenta
+    }
+    
+    def format(self, record):
+        color = self.COLOR_MAP.get(record.levelno, Fore.WHITE)  # Default to white
+        log_msg = super().format(record)
+        return f"{color}{log_msg}{Style.RESET_ALL}"
 
 
 class ProjectLogger:
