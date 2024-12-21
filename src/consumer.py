2119b5f40071ee46d4d119c5b9dac8ffecc6243b
Yunus Can Cengiz <yunuscancengizz@gmail.com>
2024-12-21 03:30:14 +0300

    bug fix

--- src/consumer.py
+++ src/consumer.py
@@ -63,6 +63,7 @@ class SimpleConsumer:
                         break
                 msg = self.deserialize_data(data=msg.value())
                 #self.logger.info(msg=f'Consumed message: {msg}')
+                print(f'Bucket: {self.influx_bucket}')
                 self.influx_db_client.write_into_influxdb(bucket=self.influx_bucket, data=msg)
                 
             except KeyboardInterrupt:
